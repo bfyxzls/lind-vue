e909b23fb44750840f18e0ab8652bdf59f0d46fc
bfyxzls <bfyxzls@sina.com>
2019-11-19 22:29:38 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,17 @@
   <div id="app">
   <layout></layout>
     <login></login> // 显示组件
-    <list></list>
   </div>
 </template>
-
 
 <script>
  
 import login from './components/login' // 引入login.vue组件
-import list from './components/list' 
 import layout from './components/layout'
 export default {
   name: 'App',
   components: {
     'login': login,
-    'list': list,
     'layout':layout
   }
 }
