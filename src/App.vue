62ad1d6b1768335d8a46c0700687ba7539e2cd3f
zhangzhanling <zhangzhanling@chinalawinfo.com>
2019-11-20 17:49:24 +0800

    add data

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
 <template>
   <div id="app">
   <layout></layout>
-    <login></login> // 显示组件
+   <data></data>
   </div>
 </template>
 
 <script>
  
-import login from './components/login' // 引入login.vue组件
 import layout from './components/layout'
+import data from './components/data'
 export default {
   name: 'App',
   components: {
-    'login': login,
-    'layout':layout
+    'layout':layout,
+    'data':data
   }
 }
  
---
+++ src/components/data.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="app-cont">
+            <div class="vue-resource">
+            <h3>获取其他地址里的json数据并进遍历</h3>
+            <ul>
+                <li v-for="item in arrs">
+                    <!-- {{item}} -->
+                    <span class="id">{{item.userId}} </span>
+                    <span class="title">{{item.title}}</span>
+                    <span class="completed" v-show="item.completed">选中</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        name: "app-cont",
+        data () {
+            return {
+                arrs:{}
+            }
+        },
+        methods: {
+        },
+        // 页面加载之前，用created钩子函数-获取网页数据
+        created() {
+            this.$http.get("http://jsonplaceholder.typicode.com/todos")
+            .then((data) => {
+                alert(data);
+                this.arrs = data.body;
+            })
+        }
+    }
+</script>
