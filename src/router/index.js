e600bd9ae5a780db62aa42f61cdda5f7e1239c34
zhangzhanling <zhangzhanling@chinalawinfo.com>
2019-11-21 11:03:18 +0800

    更新了多页面VUE，添加了统一的模板

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import HelloWorld from '@/views/HelloWorld'
+import Login from '@/views/Login'
+import List from '@/views/List'
+import DataList from '@/views/DataList'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',//http://localhost:8080/#/hello
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path: '/login', //http://localhost:8080/#/login
+      name: 'Login',
+      component: Login
+    },{
+      path: '/list', //http://localhost:8080/#/list
+      name: 'List',
+      component: List
+    },{
+      path: '/data', //http://localhost:8080/#/data
+      name: 'DataList',
+      component: DataList
     }
   ]
 })
---
+++ src/views/dataList.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="app-cont">
+            <div class="vue-resource">
+            <Alert>获取其他地址里的json数据并进遍历</Alert>
+            <!--
+            <ul>
+                <li v-for="item in arrs">               
+                    <span class="id">{{item.userId}} </span>
+                    <span class="title">{{item.title}}</span>
+                    <span class="completed" v-show="item.completed">选中</span>
+                </li>
+            </ul>
+            -->
+        <Table border ref="selection" :columns="columns4" :data="data1"></Table>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        name: "app-cont",
+        data () {
+            return {
+                 columns4: [
+                    {
+                        type: 'selection',
+                        width: 60,
+                        align: 'center'
+                    },
+                    {
+                        title: '用户ID',
+                        key: 'userId'
+                    },
+                    {
+                        title: '编号',
+                        key: 'id'
+                    },
+                    {
+                        title: '标题',
+                        key: 'title'
+                    },
+                    {
+                        title: '是否完成',
+                        key: 'completed'
+                    }
+                    
+                ],
+                data1:[]
+                
+            }
+        },
+        methods: {
+        },
+        // 页面加载之前，用created钩子函数-获取网页数据
+        created() {
+            this.$http.get("../static/dataDic.json")
+            .then((data) => {
+                var arrs=[];
+             for(var i=0;i<10;i++){
+                arrs.push(data.body[i]);
+             }
+              this.data1=arrs;
+            
+            })
+        }
+    }
+</script>
