b71f017241a3b83950a3c5daaf849d296cebde04
bfyxzls <bfyxzls@sina.com>
2020-06-25 10:15:36 +0800

    ok

--- src/views/test-grammer.vue
+++ src/views/test-grammer.vue
@@ -1,11 +1,11 @@
 <template>
-<div>
-
-  <Input type="text" v-model="message" clearable placeholder="请输入用户名" style="width: 200px" />
-  {{full}}
-  <br>
-  len: <Input type="text" v-model="len" /> width:<Input type="text" v-model="width" /> 
-  <input v-model="areas" type="text" />
+  <div>
+    <Input type="text" v-model="message" clearable placeholder="请输入用户名" style="width: 200px" />
+    {{full}}
+    <br />
+    len:  <Input type="text" v-model="len" />
+    width: <Input type="text" v-model="width" />
+    area:  <input v-model="areas" type="text" />
   </div>
 </template>
 <script>
@@ -17,10 +17,10 @@ export default {
   data() {
     // 定义变量
     return {
-          message: "hello",
-          full:"",
-          len:0,
-          width:0,
+      message: "hello",
+      full: "",
+      len: 0,
+      width: 0
     };
   },
   methods: {
@@ -31,21 +31,22 @@ export default {
   },
   created() {
     //html加载完成之前执行,执行顺序：父组件-子组件
-   },
+  },
   mounted() {
     //加载完成后执行,执行顺序：子组件-父组件
- 
   },
-  watch:{
+  watch: {
     //监听一个值的变化，然后执行相对应的函数
-       message : function(val) {this.full = "名称:"+val; }
+    message: function(val) {
+      this.full = "名称:" + val;
+    }
   },
-  computed:{
+  computed: {
     //计算属性，也就是依赖其它的属性计算所得出最后的值
     areas: function() {
-						console.log('调用computed')
-						return this.len * this.width
-					}
+      console.log("调用computed");
+      return this.len * this.width;
+    }
   }
 };
 </script>
